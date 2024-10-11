ef0ed4179876f94628b99d85ef6ee15666ddb335
csantilmuy <csantilmuy@educacion.navarra.es>
2024-10-11 22:46:09 +0200

    Update Ejercicio_03.py

--- Ejercicio_03.py
+++ Ejercicio_03.py
@@ -1,5 +1,5 @@
 #Escribir un programa que pregunte el nombre del usuario en la consola y
 # después de que el usuario lo introduzca muestre por pantalla la cadena ¡Hola
 # <nombre>!, donde <nombre> es el nombre que el usuario haya introducido.
-nombre = input("Nombre de usuario:")
-print("¡Hola" + nombre + "!")
+nombre = input("Nombre de usuario: ")
+print("¡Hola " + nombre + "!")
